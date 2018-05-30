7bf29e857aceae783bdc259c4548e4427f0471ab
Anna Khamsamran <akhamsamran1@cnm.edu>
2018-05-29 20:03:13 -0600

    executive summary in epic partially completed

---
+++ epic/executive-summary.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Executive Summary for permedsum</title>
+	</head>
+	<body>
+		<h1>Executive Summary</h1>
+		<p>Problem</p>
+		<ol>
+			<li>Every time a person goes to a new medical provider, they have to complete up to an hour of forms
+			to give the medical provider basic contact info, medical history, insurance information, etc.
+			A way to store this information and share it with each new provider at the "press of a button" or the
+			equvalent, but with security to prevent loss of personal data.</li>
+			<li>Every medical provider information is kept separately from every other for the same patient.
+			Patients must fill out information releases and request their test results and medical information
+			individually from each doctor.  The problems: 1. this is inconvenient. 2. This creates fractured care.
+			3. This results in repeating of expensive tests. 4. all of this leads to medical care falling through the cracks.
+			5. This causes great frustration for the patient, incomplete diagnosis, lack of tools for the medical practitioner.</li>
+		</ol>
+		<p>Solution</p>
+		<ol>
+			<li>A repository for all basic medical info needed for signing-up or signing-in in to a new doctor.
+			Permedsum will be a database with all basic medical info with capability to be shared with a medical provider office</li>
+			<li>A repository for all test results and medical provider history and notes.  This should also be able to
+			be shared with medical providers, as desired by the patient</li>
+		</ol>
+	</body>
+</html>
